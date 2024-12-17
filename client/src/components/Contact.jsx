015038dc294d2da7772024eb132dd46c140c8dae
pumpkin-stacks <pastacks@proton.me>
2024-12-16 21:45:15 -0600

    Contact page progress

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,38 +5,142 @@ const Contact = (props) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        phoneNumber: 0,
+        phoneNumber: '',
         eventDate: '',
-        services: '',
-        location: '',
+        services: ['Hair', 'Makeup', 'Both'],
+        streetAddress: '',
         
     })
+    
+    const handleChange = (e) => {
+        const {name, email, phoneNumber, eventDate, services, location} = e.target;
+        if (type === 'checkbox' && name === 'services') {
+            setFormData((prevState) => ({
+                ...prevState,
+                services: checked
+                ? [...prevState.services, value]
+                : prevState.services.filter((service) => service !== value),
+            }))
+        } else {
+            setFormData((prevState) => ({
+                ...prevState,
+                [name]: value,
+            }))
+        }   
+    }
 
 
     return (
         <>
-        <p>CONTACT PAGE</p>
+            <p>CONTACT PAGE</p>
             <div className='info'>
-                <form>
-                    <label>Name</label>
-                    <input type="text" />
+                <form className='form'>
 
-                    <label>Email</label>
-                    <input type="email" />
+                    <fieldset className='contactField'>
+                        <legend>Contact info</legend>
+                            <label>Name</label>
+                            <input 
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="Name"
+                            />
 
-                    <label>Phone Number</label>
-                    <input type="number" />
+                            <label>Email</label>
+                            <input 
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Email Address"
+                            />
 
-                    <label>Date of Event</label>
-                    <input type="date" />
+                            <label>Phone Number</label>
+                            <input 
+                            type="number"
+                            name="phoneNumber"
+                            value={formData.phoneNumber}
+                            onChange={handleChange}
+                            placeholder="(000)-000-0000"
+                            />
 
-                    <label>Services</label>
-                    <input type="checkbox" />
+                            <label>Date of Event</label>
+                            <input 
+                            type="date"
+                            name="date"
+                            value={formData.name}
+                            onChange={handleChange}
+                            />
+                    </fieldset>
 
-                    <label>Location</label>
-                    <input type="text" />
 
-                    
+
+
+
+                    <fieldset className="servicesField">
+                            <legend>Sevices</legend>
+
+                                <label>
+                                <input 
+                                type="checkbox" 
+                                />
+                                Hair
+                                </label>
+                                <label>
+                                How many
+                                <input type="number" />
+                                </label>
+
+                                <label>
+                                <input 
+                                type='checkbox'
+                                />
+                                Makeup
+                                </label>
+                                <label>
+                                How many
+                                <input type="number" />
+                                </label>
+
+                                <label>
+                                <input 
+                                type="checkbox" 
+                                />
+                                Hair AND Makeup
+                                </label>
+
+                                <label>
+                                How many
+                                <input type="number" />
+                                </label>
+                    </fieldset>
+
+
+
+
+                    <fieldset className='addressField'>
+                        <legend>Venue Address</legend>
+
+                            <label>Street</label>
+                            <input 
+                            type="text"
+                            name="streetAddress"
+                            value={formData.streetAddress}
+                            onChange={handleChange}
+                        />
+
+                            <label>City</label>
+                            <input type="text" />
+
+                            <label>State</label>
+                            <input type="text" />
+
+                            <label>Zip</label>
+                            <input type="text" />
+                    </fieldset>
+
+
                 </form>
             </div>
         </>
--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 const NavigationBar = (props) => {
     return (
         <>
-            <Link to='/'><button>Home</button></Link>
-            <Link to='/about'><button>About</button></Link>
-            <Link to='/portfolio'><button>Portfolio</button></Link>
-            <Link to='contact'><button>Contact</button></Link>
-            <Link to='/booking'><button>Booking</button></Link>
+            <div className='navBar'>
+                <Link to='/'><button>Home</button></Link>
+                <Link to='/about'><button>About</button></Link>
+                <Link to='/portfolio'><button>Portfolio</button></Link>
+                <Link to='contact'><button>Contact</button></Link>
+                <Link to='/booking'><button>Booking</button></Link>
+            </div>
         </>
     );
 };
